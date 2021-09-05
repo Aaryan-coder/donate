3cd561aae2109e0113c0f6312cdf4f586837abf3
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2021-09-05 08:48:31 +0530

    Add files via upload

---
+++ src/Clothes_search.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+const Clothes_search = (props) =>{
+	const {id,Type,Availability,Quantity,Donor,Contact} = props.cloth
+	const[show,setShow] = useState(false)
+	const handleClick = (id) =>{
+		setShow(!show)
+	}
+	return(
+	<div className = "card9">
+	<div key = {id}>
+					<h1 className = 'head'>Donor: {Donor}</h1>
+					<h2 className = 'head2'>Type: {Type}</h2>
+					<h2 className = 'head2' >Quantity: {Quantity}</h2>
+					<h3 className = 'head2'>Available: {Availability}</h3>
+					<h4 className = 'head2'>{show?Contact:" "}</h4>
+					<br />
+					<button type = 'submit' className = 'Find' onClick = {()=>handleClick(id)}>{show?"Show less":"Contact Donor"}</button>
+					</div>
+				</div>
+	)
+}
+
+export default Clothes_search;
---
+++ src/clothes_data.js
@@ -0,0 +1,36 @@
+const clothes_data = [
+{
+	id:1,
+	Donor: "Abhi",
+	Type: ["Shirt, ","pant"],
+	Quantity: 10,
+	Availability: "1 September 2021",
+	Contact: 1093465239
+},
+{
+	id:2,
+	Donor: "Shaun",
+	Type: ["Pant, ","Saree"],
+	Quantity: 20,
+	Availability: "1 October 2021",
+	Contact: 1009543281
+},
+{
+	id:3,
+	Donor: "Saurav",
+	Type: ["Saree, ","Shirt"],
+	Quantity: 30,
+	Availability: "1 November 2021",
+	Contact: 1122516891
+},
+{
+	id:4,
+	Donor: "Ram",
+	Type: "Sudithar",
+	Quantity: 40, 
+	Availability: "1 December 2021",
+	Contact: 5673241891
+}
+]
+
+export default clothes_data;
