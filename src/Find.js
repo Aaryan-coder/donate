d98aadc9989410cfdb6345f5edbf9e614b33fa35
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2021-09-05 08:56:47 +0530

    Update Find.js

--- src/Find.js
+++ src/Find.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {Link} from "react-router-dom"
+
+
 
 function Find(){
+	const [username,setUsername] = useState(" ")
+	const [password,setPassword] = useState(" ")
+
+	const handleSubmit = (e) =>{
+		e.preventDefault();
+		console.log(username)
+	}
+
 	return(
-		<div>
-		<h1>Find</h1>
+		<div className = 'center'>
+		<h1 className = 'dark'>Please login to Proceed.</h1>
+		<div className = 'buffer'></div>
+		<div className = "container1">
+		<div className = "card1">
+		<h2>Login Form</h2>
+		<form onSubmit = {handleSubmit}>
+		<div className = "inputBox">
+		<label htmlFor = 'username'>Username: </label>
+		<input type = 'text' 
+		id = 'username' 
+		name = 'username' 
+		value = {username} 
+		onChange = {(e)=>setUsername(e.target.value)} />
 		</div>
+		<div className = 'inputBox'>
+		<label htmlFor = 'password'>Password: </label>
+		<input type = 'password' 
+		id = 'password' 
+		name = 'password' 
+		value = {password} 
+		onChange = {(e)=>setPassword(e.target.value)} />
+		</div>
+		<div className = "inputBox">
+		<button type = 'submit'  ><Link className = "underline" to = '/Locality'><p className = "color">Login</p></Link></button></div>
+		<div className = 'break'></div>
+		<p>Not yet Registered?</p>
+		<Link className = 'underline' to = "/Register_charity"><p className = 'bright'>Register here</p></Link>
+		</form>
+		</div>
+		</div>
+		</div>
+		
 		)
 }
+
 export default Find;
+
+ 
