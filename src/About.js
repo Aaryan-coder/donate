dfe28b3fc56eb09d33d76115fee6c5c84a1ff404
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2021-09-05 08:26:16 +0530

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function About(){
+	const image = "https://images.unsplash.com/photo-1531545514256-b1400bc00f31?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YWJvdXQlMjB1c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
+
 	return(
 		<div>
-			<h1>About</h1>
+		<div className = "img-container">
+			<img src = {image} alt = "about us" className = "person-img" />
+			</div>
+			<h1 className = "about">About Us</h1>
+			<div className = 'underline1'></div>
+			<br />
+			<hr />
+			<p>We help 
 		</div>
 		)
 }
